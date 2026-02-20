0b2bfea319f285f63f1b27262f446e82d85713d3
cho hyeon jun <kanawer070800@gmail.com>
2026-02-20 19:23:45 +0900

    14 end

---
+++ linux_debug/codes/testcode/test_14.6/test3.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo 0 > /sys/kernel/debug/tracing/tracing_on
+sleep 1
+echo "tracing_off"
+
+echo 0 > /sys/kernel/debug/tracing/events/enable
+sleep 1
+echo "events disabled"
+
+echo secondary_start_kernel > /sys/kernel/debug/tracing/set_ftrace_filter
+sleep 1
+echo "set_ftrace_filter init"
+
+echo function > /sys/kernel/debug/tracing/current_tracer
+sleep 1
+echo "function tracer enabled"
+#-----------------------------------------------------------------
+echo kmem_cache_alloc_trace > /sys/kernel/debug/tracing/set_ftrace_filter
+#echo ext4_file_read_iter ext4_llseek >> /sys/kernel/debug/tracing/set_ftrace_filter
+#echo ext4_sync_file __close_fd >> /sys/kernel/debug/tracing/set_ftrace_filter
+sleep 1
+echo "set_ftrace_filter enabled"
+
+echo 1 > /sys/kernel/debug/tracing/events/kmem/kmalloc/enable
+echo 1 > /sys/kernel/debug/tracing/events/kmem/kfree/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/raw_syscalls/sys_enter/enable
+#echo 1 > /sys/kernel/debug/tracing/events/raw_syscalls/sys_exit/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable
+#echo 1 > /sys/kernel/debug/tracing/events/sched/sched_process_exit/enable
+#echo 1 > /sys/kernel/debug/tracing/events/sched/sched_wakeup/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/signal/signal_generate/enable
+#echo 1 > /sys/kernel/debug/tracing/events/signal/signal_deliver/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/workqueue/workqueue_activate_work/enable
+#echo 1 > /sys/kernel/debug/tracing/events/workqueue/workqueue_queue_work/enable
+#echo 1 > /sys/kernel/debug/tracing/events/workqueue/workqueue_execute_start/enable
+#echo 1 > /sys/kernel/debug/tracing/events/workqueue/workqueue_execute_end/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_entry/enable
+#echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_exit/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/timer/timer_init/enable
+#echo 1 > /sys/kernel/debug/tracing/events/timer/timer_start/enable
+#echo 1 > /sys/kernel/debug/tracing/events/timer/timer_expire_exit/enable
+#echo 1 > /sys/kernel/debug/tracing/events/timer/timer_cancel/enable
+#echo 1 > /sys/kernel/debug/tracing/events/timer/timer_expire_entry/enable
+
+#echo 1 > /sys/kernel/debug/tracing/events/irq/softirq_raise/enable
+#echo 1 > /sys/kernel/debug/tracing/events/irq/softirq_entry/enable
+#echo 1 > /sys/kernel/debug/tracing/events/irq/softirq_exit/enable
+
+sleep 1
+echo "event enabled"
+#-------------------------------------------------------------------
+echo 1 > /sys/kernel/debug/tracing/options/func_stack_trace
+echo 1 > /sys/kernel/debug/tracing/options/sym-offset
+echo "function stack trace enabled"
+
+echo 1 > /sys/kernel/debug/tracing/tracing_on
+echo "tracing_on"
